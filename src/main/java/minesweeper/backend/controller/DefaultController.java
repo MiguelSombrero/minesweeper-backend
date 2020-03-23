8167ef7d4b2ca94f55e154f6de16f95470ab1ee9
Miika Somero <miika.somero@gmail.com>
2020-03-23 15:18:58 +0200

    initialized project

---
+++ src/main/java/minesweeper/backend/controller/DefaultController.java
@@ -0,0 +1,13 @@
+package minesweeper.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+	@GetMapping("*")
+	public String home() {
+		return "hello!";
+	}
+}
