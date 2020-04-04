12a50722f562cfb3a8f54fa38f111d2e7eeb08e6
Miika Somero <miika.somero@gmail.com>
2020-04-04 19:07:27 +0300

    added frontend build folder

---
+++ src/main/resources/static/build/precache-manifest.a73f171d94a9749c90fcc33a9cf982f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f5e7e982911819d96539994306e26d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d110ba4a643b7ce855b",
+    "url": "/static/css/main.7ecf175e.chunk.css"
+  },
+  {
+    "revision": "3972248932b260bafc31",
+    "url": "/static/js/2.2d4fd93e.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.2d4fd93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d110ba4a643b7ce855b",
+    "url": "/static/js/main.c4f4b12b.chunk.js"
+  },
+  {
+    "revision": "042956f35592ccb10fcb",
+    "url": "/static/js/runtime-main.13607358.js"
+  }
+]);
