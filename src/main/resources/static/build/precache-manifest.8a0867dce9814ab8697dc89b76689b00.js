2a48b9bdf3eabbdd4002e04c88ad8c7693e3c3f9
Miika Somero <miika.somero@gmail.com>
2020-04-07 19:01:53 +0300

    new build for ui

---
+++ src/main/resources/static/build/precache-manifest.8a0867dce9814ab8697dc89b76689b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0675597aa828b3bb2a52032274d9d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c332c7b7225a5d3ae644",
+    "url": "/static/css/main.05b04401.chunk.css"
+  },
+  {
+    "revision": "437c18423fbfb09dcbd7",
+    "url": "/static/js/2.27b147ee.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.27b147ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c332c7b7225a5d3ae644",
+    "url": "/static/js/main.a8af23b9.chunk.js"
+  },
+  {
+    "revision": "042956f35592ccb10fcb",
+    "url": "/static/js/runtime-main.13607358.js"
+  }
+]);
--- src/main/resources/static/build/service-worker.js
+++ src/main/resources/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9409c4c7bdfe25bbb3143486767c2b13.js"
+  "/precache-manifest.8a0867dce9814ab8697dc89b76689b00.js"
 );
 
 self.addEventListener('message', (event) => {
